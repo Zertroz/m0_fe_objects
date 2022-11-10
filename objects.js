e1d022d8b5407ef514249b089156769ac902953f
Kass Daniello <zertroz@gmail.com>
2022-11-10 16:40:01 -0500

    Add assignment

--- objects.js
+++ objects.js
@@ -13,24 +13,25 @@ foods = {
 
 // Write code that prints all of the 'keys' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(foods));
 
 // Write code that prints all of the 'values' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.values(foods));
 
 // Write code that prints the value of the second food of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(foods.grapes)
 
 // Write code that adds a food to the foods object. 
 // Then, print the updated object:
-// YOUR CODE HERE
+foods.oranges = 5;
+console.log(foods.oranges)
 
 
-#-------------------
+// #-------------------
 // Part 2: Email
-#-------------------
+// #-------------------
 
 
 // Think about all the pieces of information associated with one single email in your inbox.
@@ -39,21 +40,31 @@ foods = {
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+var email = {
+  sender: "darksouls@soapstone.com",
+  senderName: "Solaire",
+  subject: "So grossly incandescant!",
+  reciever: "player@soapstone.com",
+  receiverName: "Zertroz"
+}
+
 // Write code that logs your email object to the terminal.
 
+console.log(email);
 
 // Write code that logs all of the 'keys' of the email object 
 // you created above:
-// YOUR CODE HERE
+
+console.log(Object.keys(email));
 
 // Write code that logs all of the 'values' of the email object 
 // you created above:
-// YOUR CODE HERE
 
+console.log(Object.values(email));
 
-#-------------------
+// #-------------------
 // Part 3: Many Emails - CHALLENGE!
-#-------------------
+// #-------------------
 
 // LONG EXAMPLE:
 // Now that we've learned about Objects AND Arrays, we can combine them.
@@ -99,3 +110,28 @@ console.log(posts[0]);
 // YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+var email = [
+  {
+    sender: "darksouls@soapstone.com",
+    senderName: "Solaire",
+    subject: "So grossly incandescant!",
+    receiver: "player@soapstone.com",
+    receiverName: "Zertroz"
+  },
+  {
+    sender: "gwyn@soapstone.com",
+    senderName: "Gwyn, Lord of Cinder",
+    subject: "Reignite the flame!",
+    receiver: "chosenundead@soapstone.com",
+    receiverName: "Seigmeyer of Catarina"
+  },
+  {
+    sender: "emeraldherald@soapstone.com",
+    senderName: "Shannalot",
+    subject: "Bearer- Seek- Seek- Lest",
+    receiver: "mirrahknight@soapstone.com",
+    receiverName: "Lucatiel of Mirrah"
+  }
+]
+console.log(email)
--- selfEval.md
+++ selfEval.md
@@ -1,5 +1,5 @@
 # Self Evaluation
 
-- What did you struggle with in this section?
-- How is note-taking going? What are you finding helpful? When do you find yourself taking the most helpful notes?
-- What are you noticing that you lean into - Googling, or trying to solve a problem in code on your own first?
+- What did you struggle with in this section? Some of the instructions were a little unclear, but with some *productive struggle* I got through it.
+- How is note-taking going? What are you finding helpful? When do you find yourself taking the most helpful notes? Keeping track of vocabulary and what each thing does helps the most. The actual code is difficult to write in a notebook, so I try to do that on replit.
+- What are you noticing that you lean into - Googling, or trying to solve a problem in code on your own first? Trying to solve it first. Which means I sometimes spend way more time than I should on a question, but it means I can work through my problems organically, without relying on others. Of course, if I'm not gettin anywhere with that, then I resort to Google or friends.
